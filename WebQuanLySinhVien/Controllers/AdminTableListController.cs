8ef09804480d38ad6bff4d08a5eda02792fa8f68
HuuNhuan <nhuan_dpm215412@student.agu.edu.vn>
2025-02-27 15:05:50 +0700

    Tạo AdminTableList

---
+++ WebQuanLySinhVien/Controllers/AdminTableListController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebQuanLySinhVien.Models;
+using WebQuanLySinhVien.Models.ViewModels;
+
+namespace WebQuanLySinhVien.Controllers
+{
+    public class AdminTableListController : Controller
+    {
+        private readonly QuanLySinhVienContext _context;
+
+        public AdminTableListController(QuanLySinhVienContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            // Lấy dữ liệu từ bảng Khoa và Ngành
+            var danhSachKhoa = _context.Khoas.ToList();
+            var danhSachNganh = _context.Nganhs.ToList();
+            var danhSachHocPhan = _context.Hocphans.ToList();
+
+            // Tạo ViewModel và gán dữ liệu
+            var viewModel = new AdminTableList
+            {
+                DanhSachKhoa = danhSachKhoa,
+                DanhSachNganh = danhSachNganh,
+                DanhSachHocPhan= danhSachHocPhan
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ WebQuanLySinhVien/Models/ViewModels/AdminTableList.cs
@@ -0,0 +1,9 @@
+﻿namespace WebQuanLySinhVien.Models.ViewModels
+{
+    public class AdminTableList
+    {
+        public List<Khoa>? DanhSachKhoa { get; set; }
+        public List<Nganh>? DanhSachNganh { get; set; }
+        public List<Hocphan>? DanhSachHocPhan { get; set; }
+    }
+}
